ab45b84b589b2901720997a3e5a9a78cadb3a49f
Himenon <6715229+Himenon@users.noreply.github.com>
2019-03-16 19:15:45 +0900

    refactor(condition): update

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ let modeKey: string = "mode";
 
 const IS_BROWSER = typeof window !== "undefined" && "HTMLElement" in window;
 
+if (IS_BROWSER) {
+  browseWindow = window;
+}
+
 export const setup = (params: SetupParams) => {
   browseWindow = params.browseWindow || browseWindow;
   modeKey = params.modeKey || modeKey;
@@ -17,10 +21,9 @@ export const setup = (params: SetupParams) => {
  * @param mode query params key name
  */
 export const isMode = (mode: string): boolean => {
-  if (!IS_BROWSER) {
+  if (!browseWindow) {
     return false;
   }
-  browseWindow = !!browseWindow ? browseWindow : window;
   const params = new URLSearchParams(browseWindow.location.search);
   return params.get(modeKey) === mode;
 };
